f6a18315b7acee94da7d74853c8d80019c0a36d9
Azer0s <sim17057@spengergasse.at>
2017-05-27 20:48:22 +0200

    Added sounds

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -6,13 +6,16 @@ public class Jump : MonoBehaviour
 {
     public float JumpSpeed = 240f;
     public float ForwardSpeed = 20f;
+    public AudioClip Impact;
     private Rigidbody2D _body2D;
     private InputState _inputState;
+    private AudioSource _audio;
 
 	public void Awake ()
 	{
 	    _body2D = GetComponent<Rigidbody2D>();
 	    _inputState = GetComponent<InputState>();
+	    _audio = FindObjectOfType<AudioSource>();
 	}
 	
 	void Update () {
@@ -21,6 +24,7 @@ public class Jump : MonoBehaviour
 	        if (_inputState.ActionButton)
 	        {
 	            _body2D.velocity = new Vector2(transform.position.x < 0 ? ForwardSpeed : 0, JumpSpeed);
+                _audio.PlayOneShot(Impact);
 	        }
 	    }
 	}
