3c2abe9125bc8dd2c06690b695f9017fa4068bac
Azer0s <sim17057@spengergasse.at>
2017-05-28 00:25:40 +0200

    Added menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using System;
 using System.Linq;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
---
+++ Assets/Scripts/StartScreen.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartScreen : MonoBehaviour
+{
+
+    public Button Play;
+    public Button ToggleSoundButton;
+    private bool _sound = true;
+
+	// Use this for initialization
+	void Start ()
+    {
+		Play.onClick.AddListener(PlayGame);
+        ToggleSoundButton.onClick.AddListener(ToggleSound);
+        _sound = AudioListener.volume > 0;
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameStaging");
+    }
+
+    public void ToggleSound()
+    {
+        AudioListener.volume = _sound ? 0 : 1;
+        _sound = !_sound;
+    }
+}
